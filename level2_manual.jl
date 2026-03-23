3728234dc4fd000ae4d239abc68f91df4c109192
phlaster <125278254+phlaster@users.noreply.github.com>
2026-03-23 03:00:13 +0300

    finished level2

--- level2_manual.jl
+++ level2_manual.jl
@@ -369,20 +369,130 @@ end
 #===========================================================================================
 6. Дебаг: как отладить функцию по шагам?
 =#
+using Pkg
+Pkg.add("Debugger")
+using Debugger
 
 #=
 Отладить функцию по шагам с помощью макроса @enter и точек останова
 =#
+function buggy_func()
+    E = []
+    for T in [Int, Float64, String, Bool]
+        z = zero(T)
+        @bp
+        push!(E, z)
+    end
+    return E
+end
+
+# julia> Debugger.@enter buggy_func()
+# In buggy_func() at /home/alex/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:380
+#  380  function buggy_func()
+# >381      E = []
+#  382      for T in [Int, Float64, String, Bool]
+#  383          z = zero(T)
+# ●384          @bp
+#  385          push!(E, z)
+
+# About to run: vect()
+# 1|debug> c
+# Hit breakpoint:
+# In buggy_func() at /home/alex/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:380
+#  381      E = []
+#  382      for T in [Int, Float64, String, Bool]
+#  383          z = zero(T)
+# ●384          @bp
+# >385          push!(E, z)
+#  386      end
+#  387      return E
+#  388  end
+
+# About to run: push!(Any[], 0)
+# 1|debug> c
+# Hit breakpoint:
+# In buggy_func() at /home/alex/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:380
+#  381      E = []
+#  382      for T in [Int, Float64, String, Bool]
+#  383          z = zero(T)
+# ●384          @bp
+# >385          push!(E, z)
+#  386      end
+#  387      return E
+#  388  end
+
+# About to run: push!(Any[0], 0.0)
+# 1|debug> n
+# In buggy_func() at /home/alex/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:380
+#  382      for T in [Int, Float64, String, Bool]
+#  383          z = zero(T)
+# ●384          @bp
+#  385          push!(E, z)
+# >386      end
+#  387      return E
+#  388  end
+
+# About to run: iterate(DataType[Int64, Float64, String, Bool], 3)
+# 1|debug> n
+# In buggy_func() at /home/alex/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:380
+#  380  function buggy_func()
+#  381      E = []
+# >382      for T in [Int, Float64, String, Bool]
+#  383          z = zero(T)
+# ●384          @bp
+#  385          push!(E, z)
+#  386      end
+
+# About to run: getfield((String, 4), 1)
+# 1|debug> n
+# In buggy_func() at /home/alex/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:380
+#  380  function buggy_func()
+#  381      E = []
+#  382      for T in [Int, Float64, String, Bool]
+# >383          z = zero(T)
+# ●384          @bp
+#  385          push!(E, z)
+#  386      end
+#  387      return E
+#  388  end
+
+# About to run: zero(String)
+# 1|debug> n
+# ERROR: MethodError: no method matching zero(::Type{String})
+# The function `zero` exists, but no method is defined for this combination of argument types.
+
+# Closest candidates are:
+#   zero(::Type{Union{}}, Any...)
+#    @ Base number.jl:315
+#   zero(::Type{Dates.DateTime})
+#    @ Dates ~/.julia/juliaup/julia-1.12.5+0.x64.linux.gnu/share/julia/stdlib/v1.12/Dates/src/types.jl:458
+#   zero(::Type{Dates.Date})
+#    @ Dates ~/.julia/juliaup/julia-1.12.5+0.x64.linux.gnu/share/julia/stdlib/v1.12/Dates/src/types.jl:459
+#   ...
+
+# Stacktrace:
+#  [1] buggy_func()
+#    @ Main ~/Documents/Git/Hub/lec1-intro-julia-phlaster/level2_manual.jl:383
+
+
+
+
+
+
 
 
 #===========================================================================================
 7. Профилировщик: как оценить производительность функции?
 =#
 
+using Pkg
+Pkg.add("ProfileView")
+using ProfileView
 #=
 Оценить производительность функции с помощью макроса @profview,
 и добавить в этот репозиторий файл со скриншотом flamechart'а
 =#
+
 function generate_data(len)
     vec1 = Any[]
     for k = 1:len
@@ -395,10 +505,21 @@ function generate_data(len)
 end
 
 @time generate_data(1_000_000);
+# 0.605761 seconds (7.00 M allocations: 203.778 MiB, 7.42% gc time)
+ProfileView.@profview generate_data(10^6)
 
 
 # Переписать функцию выше так, чтобы она выполнялась быстрее:
+function generate_data_optimized(len)
+    vec = randn(len)
+    sort!(vec)
+    E = (sum(vec) / len)
+    @. vec ^ 3 - E
+end
 
+@time generate_data_optimized(1_000_000);
+# 0.012209 seconds (15 allocations: 19.092 MiB)
+ProfileView.@profview generate_data_optimized(10^6)
 
 #===========================================================================================
 8. Отличия от матлаба: приращение массива и предварительная аллокация?
@@ -409,16 +530,38 @@ end
 и для каждой точки входного (x) и выходного (y) выходного массива вычисляет:
 y[i] = x[i] - x[i-1]
 =#
+function diff_alloc(x::Vector{T}) where T<:Number
+    n = length(x)
+    n < 2 && (return T[])
+    [x[i+1] - x[i] for i in 1:(n - 1)]
+end
+
 
 #=
 Аналогичная функция, которая отличается тем, что внутри себя не аллоцирует новый массив y,
 а принимает его первым аргументом, сам массив аллоцируется до вызова функции
 =#
+function diff_nalloc!(y::Vector{T}, x::Vector{T}) where T<:Number
+    n = length(x)
+    length(y) == n-1 || throw(DimensionMismatch())
+    @inbounds for i in 1:(n-1)
+        y[i] = x[i+1] - x[i]
+    end
+end
+
 
 #=
 Написать код, который добавляет элементы в конец массива, в начало массива,
 в середину массива
 =#
+arr = [1, 2, 3]
+
+push!(arr, 4)
+
+pushfirst!(arr, 0)
+
+insert!(arr, 3, 10)
+
 
 
 #===========================================================================================
@@ -433,25 +576,79 @@ y[i] = x[i] - x[i-1]
 воспользоваться обеими функциями вне модуля
 =#
 module Foo
-    #export ?
+    export f_exported
+    public f_public
+    
+    f_exported(x) = x^2
+    f_public(x) = x^3
 end
-# using .Foo ?
-# import .Foo ?
 
+using .Foo
+
+f_exported(2)
+Foo.f_public(2)
 
 #===========================================================================================
 10. Зависимости, окружение и пакеты
 =#
 
 # Что такое environment, как задать его, как его поменять во время работы?
+#=
+Окружение в Julia определяется файлами Project.toml и Manifest.toml.
+Project.toml хранит список прямых зависимостей и версию проекта.
+Manifest.toml хранит точные версии всех зависимостей для воспроизводимости.
+
+Активация окружения выполняется через пакетный менеджер:
+using Pkg
+Pkg.activate("путь/к/проекту")
+
+Или при запуске Julia через флаг --project (см. раздел 12).
+Смена окружения во время работы возможна повторным вызовом Pkg.activate().
+=#
 
 # Что такое пакет (package), как добавить новый пакет?
+#=
+Пакет — это модуль Julia с определенной структурой файлов (src, test, Project.toml),
+доступный для установки через реестр (например, General) или по URL на репозиторий.
+
+Добавление пакета в активное окружение:
+using Pkg
+
+Pkg.add("Example") # Add a package from registry
+Pkg.add("Example", target=:weakdeps) # Add a package as a weak dependency
+Pkg.add("Example", target=:extras) # Add a package to the `[extras]` list
+Pkg.add("Example"; preserve=Pkg.PRESERVE_ALL) # Add the `Example` package and strictly preserve existing dependencies
+Pkg.add(name="Example", version="0.3") # Specify version; latest release in the 0.3 series
+Pkg.add(name="Example", version="0.3.1") # Specify version; exact release
+Pkg.add(url="https://github.com/JuliaLang/Example.jl", rev="master") # From url to remote gitrepo
+Pkg.add(url="/remote/mycompany/juliapackages/OurPackage") # From path to local gitrepo
+Pkg.add(url="https://github.com/Company/MonoRepo", subdir="juliapkgs/Package.jl") # With subdir
+
+Или в режиме REPL: ] add PackageName
+=#
 
 # Как начать разрабатывать чужой пакет?
+#=
+Для внесения изменений в существующий пакет используйте режим разработки.
+Это создает симлинк на локальную копию пакета вместо загрузки компилированного кода.
+
+Pkg.develop("PackageName") # Для пакета из реестра
+Pkg.develop(url="https://github.com/user/Repo.git") # Для репозитория
+
+Или в режиме REPL: ] dev PackageName
+
+После этого изменения в локальной копии пакета применяются немедленно после перезагрузки модуля.
+=#
 
 #=
 Как создать свой пакет?
 (необязательно, эксперименты с PkgTemplates проводим вне этого репозитория)
+
+using PkgTemplates
+t = Template()
+t("MyPackage")
+
+Это создаст директорию MyPackage с корректной структурой, CI/CD настройками и тестами.
 =#
 
 
@@ -459,11 +656,40 @@ end
 11. Сохранение переменных в файл и чтение из файла.
 Подключить пакеты JLD2, CSV.
 =#
+using Pkg
+Pkg.add(["JLD2", "CSV"])
+using JLD2, CSV
 
 # Сохранить и загрузить произвольные обхекты в JLD2, сравнить их
+begin
+    data_orig = Dict("a" => 1, "b" => [1.0, 2.0], "c" => randn, "d" => Main)
+    
+    filename = tempname(suffix=".jld2")
+
+    save(filename, data_orig)
+    
+    data_loaded = load(filename)
+    
+    @assert data_orig == data_loaded
+end
+
+
+
+Pkg.add("DataFrames")
+using DataFrames
 
 # Сохранить и загрузить табличные объекты (массивы) в CSV, сравнить их
+begin
+    df_orig = DataFrame(A = 1:5, B = rand(5), C = ["x", "y", "z", "w", "v"])
+    
+    filename = tempname(suffix=".jld2")
 
+    CSV.write(filename, df_orig)
+    
+    df_loaded = CSV.read(filename, DataFrame)
+
+    @assert df_orig == df_loaded
+end
 
 #===========================================================================================
 12. Аргументы запуска Julia
@@ -471,12 +697,22 @@ end
 
 #=
 Как задать окружение при запуске?
+
+Использование флага --project
+julia --project=/путь/к/проекту script.jl
+Или активация текущего директория как проекта:
+julia --project=. script.jl
 =#
 
 #=
 Как задать скрипт, который будет выполняться при запуске:
 а) из файла .jl
+julia script.jl arg1 arg2
+
 б) из текста команды? (см. флаг -e)
+julia -e "println(2 + 2)"
+julia -E "randn()"
+julia -e "using Pkg; Pkg.status()"
 =#
 
 #=
