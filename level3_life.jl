ce40298d678ae703e9e4cde2b211d65b188eabd7
phlaster <125278254+phlaster@users.noreply.github.com>
2026-03-23 03:21:03 +0300

    level3 done

--- level3_life.jl
+++ level3_life.jl
@@ -1,4 +1,3 @@
-
 module GameOfLife
 using Plots
 
@@ -7,24 +6,48 @@ mutable struct Life
     next_frame::Matrix{Int}
 end
 
+function count_neighbors(curr::Matrix{Int}, i::Int, j::Int)::Int
+    count = 0
+    rows, cols = size(curr)
+    
+    for di in -1:1, dj in -1:1
+        di == 0 && dj == 0 && continue
+        
+        ni = mod1(i + di, rows)
+        nj = mod1(j + dj, cols)
+        
+        count += curr[ni, nj]
+    end
+    
+    return count
+end
+
 function step!(state::Life)
     curr = state.current_frame
     next = state.next_frame
+    rows, cols = size(curr)
 
-    #=
-    TODO: вместо случайного шума
-    реализовать один шаг алгоритма "Игра жизнь"
-    =#
-    for i in 1:length(curr)
-        curr[i] = rand(0:1)
+    for i in 1:rows, j in 1:cols
+        neighbors = count_neighbors(curr, i, j)
+        cell = curr[i, j]
+        
+        if cell == 1
+            if neighbors < 2 || neighbors > 3
+                next[i, j] = 0
+            else
+                next[i, j] = 1
+            end
+        else
+            if neighbors == 3
+                next[i, j] = 1
+            else
+                next[i, j] = 0
+            end
+        end
     end
 
-    # Подсказка для граничных условий - тор:
-    # julia> mod1(10, 30)
-    # 10
-    # julia> mod1(31, 30)
-    # 1
-
+    copy!(curr, next)
+    
     return nothing
 end
 
@@ -41,6 +64,8 @@ function (@main)(ARGS)
         heatmap(cr)
     end
     gif(anim, "life.gif", fps = 10)
+
+    return nothing
 end
 
 export main
